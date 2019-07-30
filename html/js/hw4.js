e6750d501437960e4cf8c018afae3f58be8977d1
alengrg98 <alengeorgeshibu@cs.ajce.in>
2019-07-30 08:37:07 +0530

    commit1 to new repository

---
+++ html/js/hw4.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#nav1").click(function(){
+        $("#cont1").slideDown();
+    });
+    $("#nav2").click(function(){
+        $("#cont1").slideUp();
+    })
+})
